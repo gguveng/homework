21170843fa89f2160253fc53f39d029d9f7f48d6
gguveng <76444139+gguveng@users.noreply.github.com>
2020-12-27 23:43:10 +0300

    Create homework.py

---
+++ homework.py
@@ -0,0 +1,8 @@
+value1 = input('Please enter a value:')
+value2 = input('Please enter a value:')
+value3 = input('Please enter a value:')
+value4 =input('Please enter a value:')
+value5= input('Please enter a value:')
+
+print(f'Value1:{value1},Value2:{value2},Value3:{value3},Value4:{value4},Value5:{value5}')
+print(' Value1 type {} , Value2 type {} ,  Value3 type {} ,  Value4 type {} ,  Value5 type {}'.format(type(value1),type(value2),type(value3),type(value4),type(value5)))
